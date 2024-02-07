d6543e9fee4be6255a4401e50651324959d57d9e
Juliebambou <bocquenet.julie@gmail.com>
2024-02-07 14:35:24 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require("node-fetch");
+
+
+
+router.get('/movies', (req, res) => {
+
+
+
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.OWM_API_KEY}`
+        }
+    };
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => res.json({movies: json.results}))
+        .catch(err => console.error('error:' + err));
+})
+
+
+
+
+module.exports = router;
